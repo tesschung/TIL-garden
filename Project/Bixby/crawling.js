f7e0d3ebdd89c9c59046032c63a2a0b99f975ac1
tesschung <geobera0910@naver.com>
2019-10-02 00:00:53 +0900

    Bixby Language

---
+++ Project/Bixby/crawling.js
@@ -0,0 +1,7 @@
+import request from 'request'
+import cheerio from 'cheerio'
+import URL from 'url-parse'
+
+// var request = require('request');
+// var cheerio = require('cheerio');
+// var URL = require('url-parse');
